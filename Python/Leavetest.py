4f52385d0d208d96b97eb2071fdc73750cdef429
22767venkat <111864124+22767venkat@users.noreply.github.com>
2022-08-25 19:29:13 +0530

    Delete Leavetest.py

--- Python/Leavetest.py
+++
@@ -1,7 +0,0 @@
-from leave import Leave
-from basketleave import BasketOfLeave
-from restrictedleave import RestrictedLeave
-r1=RestrictedLeave(22767,"venkat",25,"2000-10-30")
-b1=BasketOfLeave(22767,"venkat",25,"Sick Leave")
-print(b1.display())
-print(b1.displayleave())
