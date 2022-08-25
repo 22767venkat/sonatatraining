a7e245ffbadd253fa5771c8c3045f58b87d44cd3
22767venkat <111864124+22767venkat@users.noreply.github.com>
2022-08-25 19:30:53 +0530

    Delete main.py

--- Python/main.py
+++
@@ -1,8 +0,0 @@
-player1=input("p1 turn")
-player2=input("p2 turn")
-if (player1 == "Rock" and player2=="Scissors") or(player1 == "Scissors" and player2=="paper") or ( player1 == "paper" and player2 == "Rock"):
-    print("Player1 won")
-elif(player1==player2):
-    print("It is a tie Break")
-else:
-    print("Player2 won")
